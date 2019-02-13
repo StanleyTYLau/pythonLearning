d6da56fcb8ba35b21f31867f934185d45e476ea9
Stanley Lau <stanley.tyl@gmail.com>
2019-02-13 16:47:41 +0000

    looping practice

---
+++ loops.py
@@ -0,0 +1,29 @@
+actors = [
+  "Nathan Fillion",
+  "Gina Torres",
+  "Alan Tudyk",
+  "Morena Baccarin",
+  "Adam Baldwin",
+  "Jewel Staite",
+  "Sean Maher",
+  "Summer Glau",
+  "Ron Glass"
+]
+
+roles = [
+  "Captin Malcolm Reynolds",
+  "Zoe Washbuirn",
+  "Hoban Wasburn",
+  "Inara Serra",
+  "Jayne Cobb",
+  "Kayless Fyre",
+  "Dr. Simon",
+  "RIver Tam",
+  "Derrial Book"
+]
+
+print("Featiring: \n =-=-=-=-=-= \n")
+
+for index in range(len(actors)):
+  print(actors[index], "as", roles[index])
+
